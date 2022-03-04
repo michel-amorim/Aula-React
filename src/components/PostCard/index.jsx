d4aa559a82086462530ad263b5d139fe59c061a7
Michel Amorim <michelmsa7799@gmail.com>
2022-03-04 19:59:51 -0300

    props e organização dos componentes

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,9 +1,11 @@
-export const PostCard = ({ post }) => (
+import "./styles.css";
+
+export const PostCard = ({ title, body, cover, id }) => (
   <div className="post">
-    <img src={post.cover} alt={post.title} />
+    <img src={cover} alt={title} />
     <div className="post-content">
-      <h2>{post.title}</h2>
-      <p>{post.body}</p>
+      <h2>{title}</h2>
+      <p>{body}</p>
     </div>
   </div>
 );
--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,9 +1,17 @@
+import "./styles.css";
+
 import { PostCard } from "../PostCard";
 
 export const Posts = ({ posts }) => (
   <div className="posts">
     {posts.map((post) => (
-      <PostCard key={post.id} post={post} />
+      <PostCard
+        key={post.id}
+        title={post.title}
+        body={post.body}
+        id={post.id}
+        cover={post.cover}
+      />
     ))}
   </div>
 );
